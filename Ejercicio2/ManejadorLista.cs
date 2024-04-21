3a4e63d9314576f465f87f558693bf9e3a062b7a
Reene <reenecondori0212@gmail.com>
2024-04-21 18:26:01 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio2/ManejadorLista.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    public class ManejadorLista
+    {
+        private List<int> lista = new List<int>();
+
+        public void AgregarElemento(int elemento)
+        {
+            lista.Add(elemento);
+        }
+
+        public void EliminarElemento()
+        {
+            try
+            {
+                if (lista.Count == 0)
+                {
+                    throw new InvalidOperationException("Operacion invalida en lista vacia");
+                }
+
+                lista.RemoveAt(lista.Count - 1);
+                Console.WriteLine("Elemento eliminado correctamente.");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Fin del proceso.");
+            }
+        }
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ejercicio2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ManejadorLista manejador = new ManejadorLista();
+
+        manejador.AgregarElemento(10);
+        manejador.AgregarElemento(20);
+        manejador.AgregarElemento(30);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Intentando eliminar elemento {i + 1}");
+            manejador.EliminarElemento();
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
